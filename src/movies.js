bfc0262c079579fe72466e23f7b5621c62673b5a
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-06-14 17:53:09 -0400

    iteration 6 complete

--- src/movies.js
+++ src/movies.js
@@ -10,8 +10,8 @@ function getAllDirectors(array) {
 }
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(array) {
-  let stevenMovies = array.filter((eachMovie)=>{
-    return eachMovie.director === 'Steven Spielberg' && eachMovie.genre.includes('Drama') 
+  let stevenMovies = array.filter((eachMovie) => {
+    return eachMovie.director === 'Steven Spielberg' && eachMovie.genre.includes('Drama')
   })
   return stevenMovies;
 }
@@ -19,9 +19,9 @@ function howManyMovies(array) {
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(array) {
 
-  let sum = array.reduce(( acc, val) => {
+  let sum = array.reduce((acc, val) => {
     return acc + val.score;
-  }, 0); 
+  }, 0);
 
   let avg = sum / array.length
   return Number(avg.toFixed(2));
@@ -39,13 +39,27 @@ function dramaMoviesScore(array) {
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(array) {
-  return array.sort((a, b)=> {
+  return array.sort((a, b) => {
     return a.year - b.year
-  }) 
+  })
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+// function orderAlphabetically(array) {
+//   let variable = array.sort((a, b) => {
+//     if (a.title < b.title) {
+//       return -1
+//     } else if (a.title > b.title) {
+//       return 1
+//     } else {
+//       return 0
+//     }
+//   })
+// return variable.slice(0,20)
+// }
+
+const orderAlphabetically = (array) => array.sort((a,b)=> a.title.localeCompare(b.title)).slice(0,20)
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
